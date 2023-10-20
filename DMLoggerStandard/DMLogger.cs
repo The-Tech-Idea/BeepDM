2b3388fd8b5998b5981ee9ab3867108e11f00bd6
fahad@thetechidea.com <fahad@thetechidea.com>
2023-10-20 20:48:52 +0300

    update

--- DMLoggerStandard/DMLogger.cs
+++ DMLoggerStandard/DMLogger.cs
@@ -1,16 +1,18 @@
 ﻿
 using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
+using Serilog.Sinks.TextWriter;
 using System;
 using System.ComponentModel;
 
 namespace TheTechIdea.Logger
 {
+    public delegate void LogEventHandler(string logMessage);
     public class DMLogger : IDMLogger
     {
 
         public event EventHandler<string> Onevent;
+      
+        public static event LogEventHandler LogEvent;
         public event PropertyChangedEventHandler PropertyChanged;
         private bool logstatus = true;
         public DMLogger()
@@ -18,6 +20,7 @@ namespace TheTechIdea.Logger
             logger = new LoggerConfiguration()
              .WriteTo.File("log.txt", outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
             .WriteTo.Console()
+           
             .CreateLogger();
             WriteLog("Init Logger");  //File("log.txt"
            // Log.CloseAndFlush();
@@ -27,8 +30,8 @@ namespace TheTechIdea.Logger
         {
             if (logstatus) {
                 logger.Information(info);
-
-               // Onevent?.Invoke(this, info);
+              
+                 Onevent?.Invoke(this, info);
             }
             
            
